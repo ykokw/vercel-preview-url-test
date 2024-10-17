68e20dd253f1db6e2912a73fac03ef1df9589499
Yuki Okawa <0k4w4.yuk1@gmail.com>
2024-10-17 10:38:17 +0900

    feat: use vercel deployment preview for playwright test

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,16 +8,14 @@ import { defineConfig, devices } from "@playwright/test";
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-// const webServerConfig = process.env.CI
-//   ? {}
-const webServerConfig = {
-  webServer: {
-    command: "npm run dev",
-    url: "http://localhost:3000",
-    reuseExistingServer: !process.env.CI,
-    timeout: 120 * 1000,
-  },
-};
+const webServerConfig = process.env.CI
+  ? {}
+  : {
+      command: "npm run dev",
+      url: "http://localhost:3000",
+      reuseExistingServer: !process.env.CI,
+      timeout: 120 * 1000,
+    };
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -38,7 +36,9 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:3000",
+    baseURL: process.env.CI
+      ? process.env.UI_TEST_TARGET_URL
+      : "http://localhost:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
