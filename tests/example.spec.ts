fa267b03db50d9baa909c47d9a6b33343bdc42be
Yuki Okawa <0k4w4.yuk1@gmail.com>
2024-11-05 15:51:40 +0900

    add inprogress status

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,6 +3,6 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('/');
 
-  await expect(page).toHaveTitle(/Create Next App!/);
+  await expect(page).toHaveTitle(/Create Next App！/);
   // await expect(page).toHaveTitle(/Create Next App/);
 });
