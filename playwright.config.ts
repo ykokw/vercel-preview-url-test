4dbba0b906a58f5edde73d3a0f172f0501b9ffbb
Yuki Okawa <0k4w4.yuk1@gmail.com>
2025-08-26 15:58:21 +0900

    Updated reporter setting

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI
-    ? [["github"], ["html"], ["json", { outputFile: "results.json" }]]
+    ? [["github"], ["html"], ["blob"], ["json", { outputFile: "results.json" }]]
     : "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
